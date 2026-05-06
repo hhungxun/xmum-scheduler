7999a8627585482c9f3aed45bfeccfdff1a0184e
hhungxun <hhungxunqaq@gmail.com>
2026-05-06 09:34:08 +0800

    fix: set WEBKIT_DISABLE_DMABUF_RENDERER on Linux to fix Wayland EGL crash

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // Fix WebKitGTK EGL crash on Wayland (Arch, Fedora, etc.)
+    #[cfg(target_os = "linux")]
+    {
+        if std::env::var("WEBKIT_DISABLE_DMABUF_RENDERER").is_err() {
+            std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        }
+    }
+
     xmum_scheduler_lib::run();
 }
